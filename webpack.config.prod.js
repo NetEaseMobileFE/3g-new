95175255dab19d0c9991be4188d2412fc18a138c
dYb <duanyubin2012@gmail.com>
2016-08-27 11:03:09 +0800

    webpack: Change chunkhash to contenthash for extracted files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,8 +1,8 @@
-'use strict'
 const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'))
 const entry = packageJson.pages.reduce((prev, curr) => {
   return Object.assign(prev, {
@@ -24,32 +24,32 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader'
       }, {
         test: /\.css$/,
         exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract("style", "css!postcss")
+        loader: ExtractTextPlugin.extract('style', 'css!postcss')
       }, {
         test: /\.scss$/,
         exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract("style", "css!postcss!sass")
+        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
       }, {
         test: /\.less$/,
         exclude: /node_modules/,
-        loader: ExtractTextPlugin.extract("style", "css!postcss!less")
+        loader: ExtractTextPlugin.extract('style', 'css!postcss!less')
       }, {
         test: /\.png|jpe?g|gif$/,
-        loader: "url-loader?limit=5000&name=img/[hash].[ext]",
+        loader: 'url-loader?limit=5000&name=img/[hash].[ext]',
         include: path.join(__dirname, 'src/img')
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin("css/[name].[chunkhash].css"),
+    new ExtractTextPlugin('css/[name].[contenthash].css'),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': 'production'
+        NODE_ENV: 'production'
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
@@ -59,9 +59,9 @@ module.exports = {
     }),
     new webpack.NoErrorsPlugin()
   ],
-  postcss: function() {
+  postcss: () => {
     return [
-      require("postcss-cssnext")({
+      require('postcss-cssnext')({
         browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'not ie <= 8', 'Android >= 4.0']
       })
     ]
